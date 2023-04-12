0d6b686a34b6fa849deabb909d7931312879011e
wjteng <33016809+wjteng@users.noreply.github.com>
2023-04-12 11:41:48 +0800

    Update Speck.py

--- Speck.py
+++ Speck.py
@@ -26,12 +26,12 @@ def ror(x,k):
 
 def enc_one_round(p, k):
     #Original
-    #c0, c1 = p[0], p[1];
-    #c0 = ror(c0, ALPHA());
-    #c0 = (c0 + c1) & MASK_VAL;
-    #c0 = c0 ^ k;
-    #c1 = rol(c1, BETA());
-    #c1 = c1 ^ c0;
+    c0, c1 = p[0], p[1];
+    c0 = ror(c0, ALPHA());
+    c0 = (c0 + c1) & MASK_VAL;
+    c0 = c0 ^ k;
+    c1 = rol(c1, BETA());
+    c1 = c1 ^ c0;
     
     #moved key
     #c0, c1 = p[0], p[1];
@@ -75,12 +75,12 @@ def enc_one_round(p, k):
     #c1 = c1 ^ c0;
     
     # Key add on left to right branch only
-    c0, c1 = p[0], p[1];
-    c0 = ror(c0, ALPHA());
-    c0 = (c0 + c1) & MASK_VAL;
-    ctemp = c0 ^ k;
-    c1 = rol(c1, BETA());
-    c1 = c1 ^ ctemp;
+    #c0, c1 = p[0], p[1];
+    #c0 = ror(c0, ALPHA());
+    #c0 = (c0 + c1) & MASK_VAL;
+    #ctemp = c0 ^ k;
+    #c1 = rol(c1, BETA());
+    #c1 = c1 ^ ctemp;
     
     return(c0,c1);
 
