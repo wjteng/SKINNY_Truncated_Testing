931a3400a855c7761bb0f86439c6e12f660a375b
wjteng <33016809+wjteng@users.noreply.github.com>
2024-02-28 09:05:19 +0800

    Update train_nets.py

--- train_nets.py
+++ train_nets.py
@@ -69,6 +69,7 @@ def train_distinguisher(num_epochs,diff = (0,0,0,0x0001), num_rounds=7, depth=1,
         inactive_count += 1
     
     print("inactive_count:",inactive_count)
+    print("active_count:",(16-inactive_count))
     #create the network
     net = make_resnet(depth=depth, reg_param=10**-5,inactive_count=inactive_count);
     net.compile(optimizer='adam',loss='mse',metrics=['acc']);
@@ -79,24 +80,25 @@ def train_distinguisher(num_epochs,diff = (0,0,0,0x0001), num_rounds=7, depth=1,
     
     #generate training and validation data 
     X, Y = cipher.real_differences_data(10**6,num_rounds,diff,1);
+    print(X.shape)
     for index, value in enumerate(reversed(trunc)) :
       # removing the active nibbles, leaving only inactive nibbles
-      if value == 1:
+      if value == 0:
         X = np.delete(X,slice((15-index)*4+64,(15-index)*4+4+64),1)
     for index, value in enumerate(reversed(trunc)) :
       # removing the active nibbles, leaving only inactive nibbles
-      if value == 1:
+      if value == 0:
         X = np.delete(X,slice((15-index)*4,(15-index)*4+4),1)
 
         
     X_eval, Y_eval = cipher.real_differences_data(10**5, num_rounds,diff,1);
     for index, value in enumerate(reversed(trunc)) :
       # removing the active nibbles, leaving only inactive nibbles
-      if value == 1:
+      if value == 0:
         X_eval = np.delete(X_eval,slice((15-index)*4+64,(15-index)*4+4+64),1)
     for index, value in enumerate(reversed(trunc)) :
       # removing the active nibbles, leaving only inactive nibbles
-      if value == 1:
+      if value == 0:
         X_eval = np.delete(X_eval,slice((15-index)*4,(15-index)*4+4),1)
   
     #set up model checkpoint
