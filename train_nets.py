4613e943fe99944766d60bf78b0c186f0857969a
wjteng <33016809+wjteng@users.noreply.github.com>
2024-03-05 10:24:18 +0800

    Update train_nets.py

--- train_nets.py
+++ train_nets.py
@@ -34,6 +34,8 @@ def make_resnet(num_blocks=1, num_filters=32, num_outputs=1, d1=64, d2=64, word_
   #rs = Reshape((active_count,4))(inp);
   rs = Reshape((2,inactive_count*4))(inp)
   num_filters = inactive_count*2*4
+  if num_filters % 64 ==0:
+    num_filters = 64
   perm = Permute((2,1))(rs);
   #add a single residual layer that will expand the data to num_filters channels
   #this is a bit-sliced layer
